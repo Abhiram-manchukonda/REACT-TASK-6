2294884e7eefb476ddc264a7566e4e12de711621
ABHIRAM MANCHUKONDA <126905662+Abhiram-manchukonda@users.noreply.github.com>
2023-11-10 16:36:27 +0530

    Add files via upload

---
+++ task6/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Abiram M</h1>
+      <p>ui/ux Developer</p>
+    </header>
+  );
+}
+
+export default Header;
